e146a90a995f7cb7a8022437eb5b4b08ec64ee85
nacerkraa <nacer.kraa@univ-constantine2.dz>
2022-07-11 13:42:26 +0100

    Update

--- script.js
+++ script.js
@@ -2,4 +2,4 @@ let name = "Hello"; // print string
 let age = 25; // print intger
 let isApproved = true; // Print Boolean
 let firstName = undefined; // print undefined var
-let selctedColor = null; //
+let selctedColor = null; // null arrgemnt

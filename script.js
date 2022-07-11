6380cb63762d9990249bef5ba886ca45c9b13adf
nacerkraa <nacer.kraa@univ-constantine2.dz>
2022-07-11 23:23:49 +0100

    Factory Function

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-// Start Js Oriented Programming
+// Factory Function
+
+function createCircle(redeus){
+    return {
+        redeus,
+        drow :
+        function drow(){
+            console.log('drow function with ' + redeus * redeus);
+        }
+        
+    };
+}
+
+const circle = createCircle(2);
+circle.drow();

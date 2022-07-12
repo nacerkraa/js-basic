bc7eba1d68e28bbda9361f43692f34c8ad210c7f
nacerkraa <nacer.kraa@univ-constantine2.dz>
2022-07-12 14:57:11 +0100

    Start classes on js

--- script.js
+++ script.js
@@ -1,12 +1,1 @@
-// Spread Operator
-
-const first = [1, 2 , 3];
-const second = [9, 8 , 7];
-
-
-const one = first.concat(second);
-
-const two = [...first,5,...second,'a'];
-
-console.log(one);
-console.log(two);
+// Difine Class on JavaScript

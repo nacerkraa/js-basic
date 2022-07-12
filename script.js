77ee56d9c131454b5028f5834a969664831bfccb
nacerkraa <nacer.kraa@univ-constantine2.dz>
2022-07-12 15:01:22 +0100

    Difine a sweet class

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-// Difine Class on JavaScript
+// Difine 
+
+class Person{
+    constructor(name){
+        this.name = name;
+    }
+
+    walk(){
+        console.log(this.name + "Is walking")
+    }
+}
+
+const per1 = new Person("Nacer");
+
+console.log(per1.name);
+
+per1.walk();

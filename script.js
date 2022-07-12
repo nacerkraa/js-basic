8baba20554a464873a1cf0bb6638ce1aa7ba071a
nacerkraa <nacer.kraa@univ-constantine2.dz>
2022-07-12 14:04:48 +0100

    Update

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
-
-const person = {
-    name:'Djalil',
-    walk(){
-        console.log(this);
-    }
+// normal function Hehehehe
+const seq = function sequre(number){
+    return number * number;
 }
 
-person.walk();
+console.log(seq(5));
+
+// Arow Function
+
+const seqr = number => number * number
+
+console.log(seqr(8));

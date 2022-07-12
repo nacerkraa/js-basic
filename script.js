ba5a0ce00e677e5635d084b74a64745c42bf4e75
nacerkraa <nacer.kraa@univ-constantine2.dz>
2022-07-12 14:21:46 +0100

    Set attribute

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 // Map Method
-const colors = ["green", "red", "black"];
+const adress = {
+    street : 'ain lahma',
+    city: 'ain mlila',
+    country:'algeria' 
 
-const allColors = colors.map(color => `<i>${color}</i>`);
+};
 
-console.log(allColors);
+const srtr = adress.street;
+const city = adress.city;
+const contry = adress.country
+
+console.log(srtr,city,contry);

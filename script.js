c7de398b9b57b5e1f21c2279da73c56fb7fc6ade
nacerkraa <nacer.kraa@univ-constantine2.dz>
2022-07-12 15:19:43 +0100

    Start Modules

--- script.js
+++ script.js
@@ -1,27 +1,2 @@
-// Inheritance
+// Modules
 
-class Person{
-    constructor(name){
-        this.name = name;
-    }
-
-    walk(){
-        console.log(this.name + "Is walking")
-    }
-}
-
-class Teacher extends Person {
-    constructor(name, lesson){
-        super(name);
-        this.lesson = lesson;
-    }
-    teach(){
-        console.log(this.name + ' Teach ' + this.lesson)
-    }
-}
-
-const p = new Teacher('Ahemd', 'math');
-const p2 = new Teacher('khlifa', 'french');
-
-p.teach();
-p2.teach();

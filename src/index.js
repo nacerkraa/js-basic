334eda6e58a7e9d39ae6437b9ccae5d8f5ca5200
nacerkraa <nacer.kraa@univ-constantine2.dz>
2022-07-13 12:50:43 +0100

    Update

--- src/index.js
+++ src/index.js
@@ -3,5 +3,6 @@ import { Teacher } from "./teacher";
 const p = new Teacher('Ahemd', 'math');
 const p2 = new Teacher('khlifa', 'french');
 
-p.teach();
-p2.teach();
+p.teach();file:///home/nacer/Documents/Programing/React/Projects/nacer-blog
+
+p2.teach();file:///home/nacer/Documents/Programing/React/Projects/nacer-blog
---
+++ src/person.js
@@ -0,0 +1,10 @@
+
+export class Person{
+    constructor(name){
+        this.name = name;
+    }
+
+    walk(){
+        console.log(this.name + "Is walking")
+    }
+}

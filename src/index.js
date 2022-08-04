2de860522df63ed7c8885842b9499de689aacab7
nacerkraa <nacer.kraa@univ-constantine2.dz>
2022-08-04 20:56:11 +0100

    restet project

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import { Teacher } from "./teacher";
-
-const p = new Teacher('Ahemd', 'math');
-const p2 = new Teacher('khlifa', 'french');
-
-p.teach();file:///home/nacer/Documents/Programing/React/Projects/nacer-blog
-
-p2.teach();file:///home/nacer/Documents/Programing/React/Projects/nacer-blog

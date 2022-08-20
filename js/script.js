f2611fe86c62d97d05b1caf5739cd7cc7ff4b98d
nacerkraa <nacer.kraa@univ-constantine2.dz>
2022-08-20 12:17:49 +0100

    update

--- js/script.js
+++ js/script.js
@@ -1,6 +1,36 @@
-import Book from "./Book";
+class Book{
+    constructor(title, auther, pages, price, status){
+        this.title = title;
+        this.age = age;
+        this.auther = auther;
+        this.pages = pages;
+        this.price = price;
+        this.status = status;
+    }
+
+    printAge() {
+        return 2022 - this.age;
+    }
+}
 
 
-const book = new Book("The land of zicola","atiab", 332, 28,true);
-let nb = book.showPageNumbers();
-console.log(book);
+const book = new Book(
+    'Brad website',
+    1997,
+    'Hasan',
+    505,
+    26.3,
+    true
+);
+
+
+const content = `
+    <div>
+    <h2>${book.title}</h2>
+    <ul>
+        <li>${book.printAge(1997)}</li>
+        <li>${book.auther}</li>
+        <li>${book.price}</li>
+    </ul>
+    </div>
+`;
